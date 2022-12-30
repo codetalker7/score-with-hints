424927432112a898168c685af95fe50f80d62134
Siddhant Chaudhary <urssidd@gmail.com>
2022-12-30 14:41:02 +0530

    Adding implementation of `RankedBandits`.

---
+++ RankedBandits.py
@@ -0,0 +1,64 @@
+import numpy as np
+import math
+from Policy import Policy
+
+class RankedBandits(Policy):
+    """
+    Class representing the Ranked Bandits Algorithm for learning rankings
+    with multi-armed bandits. This algorithm does not utilize hints.
+    """
+    def __init__(self, N, k, time_horizon):
+        super().__init__(N, k)
+        self.time_horizon = time_horizon
+        # initialize k multi-armed bandits, each with N arms
+        self.mabs = [MultiArmedBandit(self.N, self.time_horizon) for i in range(self.k)]
+        self.arms = np.zeros(shape=self.k)
+        self.predictedSet = []
+
+    def getKSet(self, hint=None):
+        selectedDocuments = np.zeros(shape=self.N, dtype=np.int_)
+        self.predictedSet = []
+        for i in range(self.k): # determining document for each rank
+            # get the predicted arm for the ith mab
+            self.arms[i] = self.mabs[i].selectArm()
+
+            if self.arms[i] in self.predictedSet:
+                # select arbitrary unselected document
+                unselectedDocumentIndex = np.argmin(selectedDocuments)
+                selectedDocuments[unselectedDocumentIndex] = 1
+                self.predictedSet.append(unselectedDocumentIndex + 1)
+            else:
+                # select the predicted arm
+                selectedDocuments[self.arms[i] - 1] = 1
+                self.predictedSet.append(self.arms[i])
+
+        return self.predictedSet
+
+    def feedReward(self, reward, hint=None):
+        self.T = self.T + 1
+
+        userClicked = False
+        for i in range(self.k):
+            ith_prediction = self.predictedSet[i]
+            if not(userClicked) and ith_prediction in reward and ith_prediction == self.arms[i]:
+                userClicked = True
+                mab_reward = 1
+            else:
+                mab_reward = 0
+            self.mabs[i].update(self.arms[i], mab_reward)
+
+class MultiArmedBandit:
+    """
+    Multi-Armed Bandit trained using the EXP3 algorithm.
+    """
+    def __init__(self, N, time_horizon):
+        self.N = N
+        self.time_horizon = time_horizon
+        self.gamma = min(1, math.sqrt((self.N*math.log(self.N))/((math.e - 1)*self.time_horizon)))
+
+    def selectArm(self):
+        pass
+
+    def update(self, arm, reward):
+        pass
+
